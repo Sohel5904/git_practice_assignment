3cbb8e7e8e8d031c4ca5d2ae4a603e42c54b6da7
Mohammad Sohel <mohammadsohelxyz@gmail.com>
2022-10-19 12:36:45 +0530

    added problems of prime and palindrome

---
+++ problem1.js
@@ -0,0 +1,30 @@
+// Problem1identifyPrime
+
+let num = 13
+
+ let count=0;
+    for (leti=1;i<=num;i++){
+        if(num%i===0){
+            count++;
+        }
+    }
+    if(count==2){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+
+//problem2checkPalindrome
+
+let N=6;
+let str=nrupul;
+
+ let new_str = "";
+  for(let i=N-1;i>=0;i++){
+      new_str = new_str + str[i];
+  }
+  if(str==new_str){
+      console.log("Yes");
+  }else{
+      console.log("No");
+  }
